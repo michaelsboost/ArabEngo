0b27e82c1e11b89b9b85ec53831ed87d89f6abb3
Michael Schwartz <mikethedj4@yahoo.com>
2016-02-15 17:54:44 -0600

    Added blank lesson files

---
+++ js/lessons.js
@@ -0,0 +1,2 @@
+// $("body").append("<a href=\"../\" class=\"gohome\">\n    <i class=\"fa fa-home\"></i>\n  </a>")
+$("body").append("<a href=\"../\" class=\"goback\">\n    <i class=\"fa fa-chevron-left\"></i>\n  </a>")
