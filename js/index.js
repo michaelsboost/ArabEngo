f5dfe06c965945982a4df27a495d18027575e558
Michael <MIchael@MichaelsMacBook.attlocal.net>
2017-07-08 13:24:01 -0500

    add hash for introduction too

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 // Check if there's a hash onload
 if (window.location.hash === "#community") {
   $("[data-open=arabengochats]").trigger('click');
+} else if (window.location.hash === "#introduction") {
+  $("[data-open=arabengochats]").trigger('click');
 }
 
 // Add hash when commuity is clicked
 $("[data-open=arabengochats]").click(function() {
   window.location.hash = "#community";
+});
+$("[data-open=introduction]").click(function() {
+  window.location.hash = "#introduction";
 });
