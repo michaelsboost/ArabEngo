de6a58759d69b89e56c02f9b1c07f5d3f4234058
Michael <MIchael@MichaelsMacBook.attlocal.net>
2017-07-15 17:46:14 -0500

    minor style adjustment

--- js/designer.js
+++ js/designer.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
   $(".comingsoon").click(function() {
     alertify.message('coming soon..');
   });
+  
+  $(".lesson-types a").on("click", function() {
+    var lessonType = $(this).find("span").text();
+    $(this).parent().parent().next().find("[data-output=questiontype]").text(lessonType);
+  });
 });
